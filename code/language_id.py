51ba703c5d8c9155483d9291bb4bc6717c587bf8
Lucy Li <luxisufeili@gmail.com>
2019-09-11 15:26:42 -0700

    lang id

--- code/language_id.py
+++ code/language_id.py
@@ -27,19 +27,26 @@ def subreddit_of_interest(line):
     return 'subreddit' in comment and 'body' in comment and \
         comment['subreddit'].lower() in reddits
     
-with open(SUBREDDITS, 'r') as inputfile: 
-    for line in inputfile: 
-        reddits.add(line.strip().lower())
-global MONTH
-MONTH = 'RC_2019-05'
-path = DATA + MONTH
-#path = DATA + 'tinyData'
-data = sc.textFile(path)
-data = data.filter(subreddit_of_interest)
-data = data.map(get_language)
-data = data.reduceByKey(lambda n1, n2: n1 + n2)
-data = data.map(lambda tup: (tup[0], Counter(tup[1]).most_common()))
-d = data.collectAsMap()
-sc.stop()
-with open(LOG_DIR + 'subreddit_langs.json', 'w') as outfile: 
-    json.dump(d, outfile)
+def id_langs(): 
+    with open(SUBREDDITS, 'r') as inputfile: 
+        for line in inputfile: 
+            reddits.add(line.strip().lower())
+    global MONTH
+    MONTH = 'RC_2019-05'
+    path = DATA + MONTH
+    #path = DATA + 'tinyData'
+    data = sc.textFile(path)
+    data = data.filter(subreddit_of_interest)
+    data = data.map(get_language)
+    data = data.reduceByKey(lambda n1, n2: n1 + n2)
+    data = data.map(lambda tup: (tup[0], Counter(tup[1]).most_common()))
+    d = data.collectAsMap()
+    sc.stop()
+    with open(LOG_DIR + 'subreddit_langs.json', 'w') as outfile: 
+        json.dump(d, outfile)
+
+def main(): 
+    id_langs()
+
+if __name__ == '__main__':
+    main()
