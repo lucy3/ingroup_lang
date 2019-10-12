882cf5f46264f5861e51ebca1e938c615005024c
Lucy Li <luxisufeili@gmail.com>
2019-10-12 12:22:24 -0700

    moving to savio

--- code/word_rarity.py
+++ code/word_rarity.py
@@ -10,9 +10,10 @@ import csv
 
 #ROOT = '/global/scratch/lucy3_li/ingroup_lang/'
 ROOT = '/data0/lucy/ingroup_lang/'
-WORD_COUNT_DIR = ROOT + 'logs/word_counts/'
+WORD_COUNT_DIR = ROOT + 'logs/old_word_counts/' # TODO: Change this!!!
 PMI_DIR = ROOT + 'logs/pmi/'
-SR_DATA_DIR = ROOT + 'subreddits2/'
+TFIDF_DIR = ROOT + 'logs/tfidf/'
+SR_DATA_DIR = ROOT + 'subreddits4/'
 LOG_DIR = ROOT + 'logs/'
 
 conf = SparkConf()
@@ -24,20 +25,15 @@ regex = re.compile('[%s]' % re.escape(string.punctuation))
 def count_words(): 
     """
     Get word counts in each subreddit
-    
-    Currently askreddit is broken
     """
     log_file = open(LOG_DIR + 'counting_log.temp', 'w')
     for filename in os.listdir(SR_DATA_DIR):  
         log_file.write(filename + '\n') 
-        path = SR_DATA_DIR + filename + '/RC_2019-05.conll'
-        # in some cases the file is missing 
-        # because it was big and was segmented
-        if not os.path.exists(path): continue
+        path = SR_DATA_DIR + filename + '/RC_2019-05'
         log_file.write('\tReading in textfile\n')
         data = sc.textFile(path)
         data = data.filter(lambda line: line.strip() != '')
-        data = data.map(lambda line: (line.strip().lower(), 1))
+        data = data.map(lambda line: (line.strip(), 1))
         log_file.write('\tReducing by key...\n') 
         data = data.reduceByKey(lambda a, b: a + b)
         df = sqlContext.createDataFrame(data, ['word', 'count'])
@@ -115,7 +111,6 @@ def count_overall_words(percent_param=0.2):
                 vocab.add(w[0])
     log_file.write("Vocab size:" + str(len(vocab)) + "\n")
     rdd1 = sc.emptyRDD()
-    all_counts = Counter()
     for filename in sorted(os.listdir(WORD_COUNT_DIR)): 
         if os.path.isdir(WORD_COUNT_DIR + filename) and '@' not in filename: 
             log_file.write(filename + '\n') 
@@ -124,16 +119,45 @@ def count_overall_words(percent_param=0.2):
             # filter to only words in our vocab
             rdd2 = rdd2.filter(lambda tup: tup[0] in vocab)
             rdd1 = rdd2.union(rdd1).reduceByKey(lambda x,y : x+y)
-            d = Counter(parquetFile.toPandas().set_index('word').to_dict()['count'])
-            all_counts += d
+    df = sqlContext.createDataFrame(rdd1, ['word', 'count'])
+    all_counts = Counter(df.toPandas().set_index('word').to_dict()['count'])
     with open(LOG_DIR + 'total_word_counts.json', 'w') as outfile: 
         json.dump(all_counts, outfile)
     log_file.write("DONE\n")
-    #df = sqlContext.createDataFrame(rdd1, ['word', 'count'])
-    #df.write.mode('overwrite').parquet(LOG_DIR + 'total_word_counts') 
     log_file.close()
-
-def word_tfidf(): 
+    
+def count_document_freq(percent_param=0.2):
+    vocab = set()
+    log_file = open(LOG_DIR + 'counting_df_log.temp', 'w') 
+    log_file.write("Getting vocab...\n")
+    for filename in sorted(os.listdir(WORD_COUNT_DIR)): 
+        if os.path.isdir(WORD_COUNT_DIR + filename) and '@' not in filename: 
+            log_file.write(filename + '\n') 
+            parquetFile = sqlContext.read.parquet(WORD_COUNT_DIR + filename + '/')
+            d = Counter(parquetFile.toPandas().set_index('word').to_dict()['count'])
+            num_top_p = int(percent_param*len(d))
+            for w in d.most_common(num_top_p): 
+                vocab.add(w[0])
+    log_file.write("Vocab size:" + str(len(vocab)) + "\n")
+    rdd1 = sc.emptyRDD()
+    all_counts = Counter()
+    for filename in sorted(os.listdir(WORD_COUNT_DIR)): 
+        if os.path.isdir(WORD_COUNT_DIR + filename) and '@' not in filename: 
+            log_file.write(filename + '\n') 
+            parquetFile = sqlContext.read.parquet(WORD_COUNT_DIR + filename + '/')
+            rdd2 = parquetFile.rdd.map(tuple)
+            # filter to only words in our vocab
+            rdd2 = rdd2.filter(lambda tup: tup[0] in vocab)
+            rdd2 = rdd2.map(lambda tup: (tup[0], 1))
+            rdd1 = rdd2.union(rdd1).reduceByKey(lambda x,y : x+y)
+    df = sqlContext.createDataFrame(rdd1, ['word', 'doc_freq'])
+    all_counts = Counter(df.toPandas().set_index('word').to_dict()['doc_freq'])
+    with open(LOG_DIR + 'doc_freqs.json', 'w') as outfile: 
+        json.dump(all_counts, outfile)
+    log_file.write("DONE\n") 
+    log_file.close()
+    
+def word_tfidf(percent_param=0.2): 
     """
     tf-idf is defined as 
     (1 + log tf)xlog_10(N/df) where N is the 
@@ -146,9 +170,29 @@ def word_tfidf():
         - docs: list of docs in order of matrix
         - words: list of words in order of matrix
     """
-    pass
+    log_file = open(LOG_DIR + 'tfidf.temp', 'w')
+    # load document frequency of words
+    docs = sorted(os.listdir(WORD_COUNT_DIR))
+    for filename in sorted(os.listdir(WORD_COUNT_DIR)): 
+        tfidf_d = {}
+        if os.path.isdir(WORD_COUNT_DIR + filename) and '@' not in filename: 
+            log_file.write(filename + '\n') 
+            parquetFile = sqlContext.read.parquet(WORD_COUNT_DIR + filename + '/')
+            d = Counter(parquetFile.toPandas().set_index('word').to_dict()['count'])
+            num_top_p = int(percent_param*len(d))
+            for w in d.most_common(num_top_p): 
+                tfidf_d[w[0]] = 0
+            new_filename = filename.replace('.txt', '')
+            with open(TFIDF_DIR + new_filename + '_' + str(percent_param) + '.csv', 'w') as outfile: 
+                sorted_d = sorted(tfidf_d.items(), key=operator.itemgetter(1))
+                writer = csv.writer(outfile)
+                writer.writerow(['word', 'tfidf', 'count'])
+                for tup in sorted_d: 
+                    writer.writerow([tup[0].encode('utf-8', 'replace'), str(tup[1]), str(d[tup[0]])])
+    log_file.write("DONE\n")
+    log_file.close()
 
-def niche_disem():
+def niche_disem(percent_param=0.2):
     """
     Altmann's metric, but for subreddits instead of users
     """
@@ -157,8 +201,8 @@ def niche_disem():
 def main(): 
     #count_words()
     #merge_counts()
-    #count_overall_words()
-    calculate_pmi()
+    count_overall_words()
+    #calculate_pmi()
     sc.stop()
 
 if __name__ == '__main__':
