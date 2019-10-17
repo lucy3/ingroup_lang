e4c6282c179cedf230709296ee343afd087b2929
Lucy Li <lucy3_li@berkeley.edu>
2019-10-17 03:13:28 -0700

    omg

--- code/bert_vectors.py
+++ code/bert_vectors.py
@@ -29,23 +29,27 @@ text = "After stealing money from the bank vault, the bank robber was seen fishi
 marked_text = "[CLS] " + text + " [SEP]"
 # SINGLE SENTENCE EXAMPLE
 tokenized_text = tokenizer.tokenize(marked_text)
+print(tokenized_text)
 indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
 segments_ids = [1] * len(tokenized_text)
-tokens_tensor = torch.tensor([indexed_tokens])
-segments_tensors = torch.tensor([segments_ids])
-model = BertModel.from_pretrained('bert-base-uncased')
+tokens_tensor = torch.tensor([indexed_tokens]).to('cuda')
+segments_tensors = torch.tensor([segments_ids]).to('cuda')
+model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
 model.eval()
+model.to('cuda')
 with torch.no_grad():
-    encoded_layers, _ = model(tokens_tensor, segments_tensors)
+    _, _, encoded_layers = model(tokens_tensor)
+print(len(encoded_layers))
+print("Batch size, tokens, hidden layer size:", encoded_layers[0].size())
 token_embeddings = [] 
 for token_i in range(len(tokenized_text)):
     hidden_layers = [] 
     for layer_i in range(1, 5):
-        # TODO MODIFY THIS SO WE JUST NEED THE LAST FOUR LAYERS
-        vec = encoded_layers[-layer_i][batch_i][token_i]
+        vec = encoded_layers[-layer_i][0][token_i]
         hidden_layers.append(vec)
     token_embeddings.append(hidden_layers)
 rep = [torch.cat((layer[0], layer[1], layer[2], layer[3]), 0) for layer in token_embeddings]
 # representation for a word is rep[word_index_in_sentence]
-print(cosine_similarity(rep[6].reshape(1, -1), rep[19].reshape(1, -1)))
-print(cosine_similarity(rep[10].reshape(1, -1), rep[6].reshape(1, -1)))
+print(cosine_similarity(rep[6].cpu().reshape(1, -1), rep[19].cpu().reshape(1, -1)))
+print(cosine_similarity(rep[10].cpu().reshape(1, -1), rep[6].cpu().reshape(1, -1)))
+
