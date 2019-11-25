e55e42df2a7d4d959a7bdff67839100de7ca2422
Lucy Li <lucy3_li@berkeley.edu>
2019-11-24 23:20:04 -0800

    stuff

--- code/cluster_vectors.py
+++ code/cluster_vectors.py
@@ -12,12 +12,17 @@ import xml.etree.ElementTree as ET
 import re
 import string
 import spacy
+from functools import partial
+from sklearn.decomposition import PCA
+import random
 
 ROOT = '/global/scratch/lucy3_li/ingroup_lang/'
 LOGS = ROOT + 'logs/'
-SEMEVAL_VECTORS = LOGS + 'semeval2013_bert'
-SEMEVAL_TEST_VECTORS = LOGS + 'semeval2013_test_bert2'
-SEMEVAL_TEST_VECTORS2 = LOGS + 'semeval2013_test_bert3'
+SEMEVAL2010_TRAIN_VECTORS = LOGS + 'semeval2010_train_bert2'
+SEMEVAL2010_TEST_VECTORS = LOGS + 'semeval2010_test_bert2'
+SEMEVAL2013_TRAIN_VECTORS = LOGS + 'semeval2013_train_bert2'
+SEMEVAL2013_TEST_VECTORS = LOGS + 'semeval2013_test_bert2'
+SEMEVAL2013_TEST_VECTORS2 = LOGS + 'semeval2013_test_bert3'
 
 def semeval_words_of_interest(line): 
     contents = line.strip().split('\t')
@@ -31,13 +36,16 @@ def get_semeval_vector(line):
     vector = np.array([[float(i) for i in contents[2].split()]])
     return (token, ([ID], vector))
 
-def kmeans_with_gap_statistic(tup): 
+def kmeans_with_gap_statistic(tup, dim_reduct=None): 
     """
     Based off of https://anaconda.org/milesgranger/gap-statistic/notebook
     """
     lemma = tup[0]
     IDs = tup[1][0]
     data = tup[1][1]
+    if dim_reduct is not None: 
+        pca = PCA(n_components=dim_reduct, random_state=0)
+        data = pca.fit_transform(data)
     nrefs = 50
     ks = range(2, 10)
     gaps = np.zeros(len(ks))
@@ -70,25 +78,31 @@ def get_data_size(tup):
     data = tup[1][1]
     return (token, data.shape)
     
-def semeval_clusters(test=False): 
+def semeval_clusters(test=False, dim_reduct=None): 
     conf = SparkConf()
     sc = SparkContext(conf=conf)
     if test: 
-        data = sc.textFile(SEMEVAL_TEST_VECTORS2) 
+        data = sc.textFile(SEMEVAL2013_TEST_VECTORS2) 
     else: 
-        data = sc.textFile(SEMEVAL_VECTORS)
+        data = sc.textFile(SEMEVAL2013_TRAIN_VECTORS)
     data = data.map(get_semeval_vector)
     data = data.reduceByKey(lambda n1, n2: (n1[0] + n2[0], np.concatenate((n1[1], n2[1]), axis=0)))
     #size = data.map(get_data_size).collectAsMap()
-    data = data.map(kmeans_with_gap_statistic)
+    data = data.map(partial(kmeans_with_gap_statistic, dim_reduct=dim_reduct))
     clustered_IDs = data.collect()
     sc.stop()
     #for token in size: 
     #    print(token, size[token])
     if test: 
-        outname = 'semeval_test_clusters'
+        if dim_reduct is not None: 
+            outname = 'semeval_test_clusters' + str(dim_reduct)
+        else: 
+            outname = 'semeval_test_clusters'
     else: 
-        outname = 'semeval_clusters'
+        if dim_reduct is not None: 
+            outname = 'semeval_clusters' + str(dim_reduct)
+        else: 
+            outname = 'semeval_clusters'
     with open(LOGS + outname, 'w') as outfile: 
         for tup in clustered_IDs: 
             IDs = tup[0]
@@ -96,7 +110,7 @@ def semeval_clusters(test=False):
             for i, ID in enumerate(IDs): 
                 small_id = ID.split('_')[-3]
                 lemma = ID.split('_')[-2]
-                outfile.write(lemma + ' ' + small_id + ' ' + str(labels[i]) + '\n')
+                outfile.write(lemma + ' ' + small_id + ' ' + lemma + str(labels[i]) + '\n')
 
 def get_IDs(line): 
     contents = line.strip().split('\t') 
@@ -168,51 +182,90 @@ def filter_semeval2013_vecs():
                 outfile.write(line)
     outfile.close()
 
+def sample_vectors(tup): 
+    IDs = tup[1][0]
+    X = tup[0][0]
+    cutoff = 500
+    if len(IDs) < cutoff: 
+        return tup
+    else: 
+        idx = sorted(random.sample(range(len(IDs)), cutoff))
+        IDs_sample = []
+        for i in idx: 
+            IDs_sample.append(IDs[i])
+    return (tup[0], (IDs_sample, X[idx,:]))
 
-def semeval2010_cluster_training(): 
+def semeval_cluster_training(semeval2010=False): 
     '''
     Input: training vectors
     note that one ID might have multiple instances of a word
     Output: cluster centroids
     '''
-    pass
+    random.seed(0)
+    conf = SparkConf()
+    sc = SparkContext(conf=conf)
+    if semeval2010: 
+        outname = 'semeval2010_centroids/'
+        data = sc.textFile(SEMEVAL2010_TRAIN_VECTORS)
+    else: 
+        outname = 'semeval2013_centroids/'
+        data = sc.textFile(SEMEVAL2013_TRAIN_VECTORS)
+    data = data.map(get_semeval_vector)
+    data = data.reduceByKey(lambda n1, n2: (n1[0] + n2[0], np.concatenate((n1[1], n2[1]), axis=0)))
+    data = data.map(sample_vectors)
+    size = data.map(get_data_size).collectAsMap() # TODO: delete
+    for key in size: # TODO: delete
+        print(key, size[key]) # TODO: delete
+    #data = data.map(kmeans_with_gap_statistic)
+    #clustered_IDs = data.collect()
+    sc.stop() 
+    #for tup in clustered_IDs: 
+    #    ID = tup[0][0]
+    #    lemma = ID.split('_')[-2]
+    #    centroids = np.array(tup[1][1])
+    #    np.save(LOGS + outname + lemma + '.npy', centroids)
 
 def evaluate_nmi(): 
     print("calculating nmi...") 
     goldpath = ROOT + 'SemEval-2013-Task-13-test-data/keys/gold/all.singlesense.key'
     labels1 = []
     labels2 = []
-    gold_labels = {}
-    my_labels = {}
+    gold_labels = defaultdict(dict)
+    my_labels = defaultdict(dict)
     with open(goldpath, 'r') as infile: 
         for line in infile:  
             contents = line.strip().split()
+            lemma = contents[0]
             instance = contents[1]
             label = contents[2].split('/')[0]
             if label not in labels1: 
                 labels1.append(label)
             i = labels1.index(label)
-            gold_labels[instance] = i
-    with open(LOGS + 'semeval_test_clusters', 'r') as infile: 
+            gold_labels[lemma][instance] = i # cluster ID is the label's index
+    with open(LOGS + 'semeval_test_clusters20', 'r') as infile: 
         for line in infile: 
             contents = line.strip().split()
+            lemma = contents[0]
             instance = contents[1]
-            label = contents[2] + contents[0]
+            label = contents[2]
             if label not in labels2: 
                 labels2.append(label)
             i = labels2.index(label)
-            my_labels[instance] = i
-    gold = []
-    mine = []
-    for k in gold_labels:
-        gold.append(gold_labels[k])
-        mine.append(my_labels[k])
-    print("NMI:", normalized_mutual_info_score(gold, mine)) 
+            my_labels[lemma][instance] = i
+    nmis = []
+    for lemma in gold_labels: 
+        gold = []
+        mine = []
+        for instance in gold_labels[lemma]:
+            gold.append(gold_labels[lemma][instance])
+            mine.append(my_labels[lemma][instance])
+        nmis.append(normalized_mutual_info_score(gold, mine, average_method='max'))
+    print("NMI:", sum(nmis)/float(len(nmis))) 
 
 def evaluate_bcubed(): 
     print("calculating bcubed...")
-    gold_labels = {}
-    my_labels = {}
+    gold_labels = defaultdict(dict)
+    my_labels = defaultdict(dict)
     labels = []
     goldpath = ROOT + 'SemEval-2013-Task-13-test-data/keys/gold/all.singlesense.key'
     gold_c = defaultdict(list)
@@ -220,34 +273,43 @@ def evaluate_bcubed():
     with open(goldpath, 'r') as infile: 
         for line in infile:  
             contents = line.strip().split()
+            lemma = contents[0]
             instance = contents[1]
             label = contents[2].split('/')[0]
             if label not in labels: 
                 labels.append(label)
             i = labels.index(label)
-            gold_labels[instance] = set([i])
+            gold_labels[lemma][instance] = set([i])
             gold_c[i].append(instance) 
-    with open(LOGS + 'semeval_test_clusters', 'r') as infile: 
+    with open(LOGS + 'semeval_test_clusters20', 'r') as infile: 
         for line in infile: 
             contents = line.strip().split()
+            lemma = contents[0]
             instance = contents[1]
-            label = contents[2] + contents[0] # label is cluster # + lemma 
-            if instance not in gold_labels: continue # only evaluate on things in gold
-            my_labels[instance] = set([label])
+            label = contents[2] 
+            if instance not in gold_labels[lemma]: continue # only evaluate on things in gold
+            my_labels[lemma][instance] = set([label])
             my_c[label].append(instance) 
     print("num gold clusters", len(gold_c), "num my clusters", len(my_c))
-    precision = bcubed.precision(my_labels, gold_labels)
-    recall = bcubed.recall(my_labels, gold_labels)
-    fscore = bcubed.fscore(precision, recall)
-    print("Precision:", precision, "Recall:", recall, "F-score:", fscore) 
+    precisions = []
+    recalls = []
+    fscores = []
+    for lemma in gold_labels: 
+        precision = bcubed.precision(my_labels[lemma], gold_labels[lemma])
+        recall = bcubed.recall(my_labels[lemma], gold_labels[lemma])
+        precisions.append(precision)
+        recalls.append(recall)
+        fscores.append(bcubed.fscore(precision, recall))
+    print("Precision:", np.mean(precisions), "Recall:", np.mean(recalls), "F-score:", np.mean(fscores))
 
 def main(): 
     #find_semeval2013_dups()
     #get_dup_mapping()
     #filter_semeval2013_vecs()
-    semeval_clusters(test=True)
-    evaluate_nmi()
-    evaluate_bcubed()
+    #semeval_clusters(test=True, dim_reduct=20)
+    semeval_cluster_training(semeval2010=True)
+    #evaluate_nmi()
+    #evaluate_bcubed()
 
 if __name__ == "__main__":
     main()
