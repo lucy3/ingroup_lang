1e92d75100161fc2852818722649b749e86c0507
Lucy Li <luxisufeili@gmail.com>
2019-12-16 12:29:19 -0800

    stats and analysis

--- code/users_sociolect_analysis.py
+++ code/users_sociolect_analysis.py
@@ -10,6 +10,9 @@ from sklearn.model_selection import cross_val_score
 from collections import defaultdict
 import os
 import pandas as pd
+from scipy.stats import zscore 
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+import statsmodels.api as sm
 root = '/data0/lucy/ingroup_lang/'
 
 def get_values(path, feature_dict): 
@@ -20,13 +23,7 @@ def get_values(path, feature_dict):
             feature_dict[contents[0].lower()].append(float(contents[1]))
     return feature_dict
 
-def predict_sociolects(sociolect_metric):
-    """
-    X is [community size, user activity, user loyalty, user density] 
-    where each row is a subreddit
-    y is # of high pmi words, # of high tf-idf words, or # of high pmi senses
-    """ 
-    print("Predicting metric " + sociolect_metric)
+def get_data(sociolect_metric): 
     feature_dict = defaultdict(list) # subreddit_name : [features]
     feature_names = []
     size_path = root + 'logs/commentor_counts/part-00000-64b1d705-9cf8-4a54-9c4d-598e5bf9085f-c000.csv'
@@ -68,7 +65,17 @@ def predict_sociolects(sociolect_metric):
     y = np.array(y)
     y_bin = np.array(y_bin)
     assert X.shape[0] == y.shape[0]
-    
+    # z-score the columns
+    X = zscore(X, axis=0)
+    return X, y, y_bin, feature_names
+
+def predict_sociolects(sociolect_metric):
+    """
+    X is [community size, user activity, user loyalty, user density] 
+    where each row is a subreddit
+    y is # of high pmi words, # of high tf-idf words, or # of high pmi senses
+    """ 
+    X, y, y_bin, feature_names = get_data(sociolect_metric)
     clf1 = LinearRegression(n_jobs=-1)
     clf1.fit(X, y)
     print("LINEAR REGRESSION")
@@ -77,9 +84,21 @@ def predict_sociolects(sociolect_metric):
         print(cf, clf1.coef_[i])
     clf2 = LinearRegression(n_jobs=-1) 
     scores = cross_val_score(clf2, X, y, cv=3)
-    print("5-fold cv R^2", scores)
-    print("5-fold cv R^2 mean:", np.mean(scores))
+    print("3-fold cv R^2", scores)
+    print("3-fold cv R^2 mean:", np.mean(scores))
     print
+    clf7 = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=0)
+    clf7.fit(X, y)
+    print("REGRESSION RF")
+    print("Weights for features:")
+    for i, cf in enumerate(feature_names): 
+        print(cf, clf7.feature_importances_[i])
+    clf8 = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=0)
+    scores = cross_val_score(clf8, X, y, cv=3)
+    print("3-fold cv R^2", scores)
+    print("3-fold cv R^2 mean:", np.mean(scores))
+    print
+
     clf3 = LogisticRegression(solver='liblinear')
     clf3.fit(X, y_bin)
     print("LOGISTIC REGRESSION")
@@ -88,15 +107,36 @@ def predict_sociolects(sociolect_metric):
         print(cf, clf3.coef_[0,i])
     clf4 = LogisticRegression() 
     scores = cross_val_score(clf4, X, y_bin, cv=3, scoring='f1_macro')
-    print("5-fold cv F1", scores)
-    print("5-fold cv F1 mean:", np.mean(scores))
+    print("3-fold cv F1", scores)
+    print("3-fold cv F1 mean:", np.mean(scores))
+    print
+
+    clf5 = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
+    clf5.fit(X, y_bin)
+    print("BINARY CLASSIFIER RF")
+    print("Weights for features:")
+    for i, cf in enumerate(feature_names): 
+        print(cf, clf5.feature_importances_[i])
+    clf6 = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
+    scores = cross_val_score(clf6, X, y_bin, cv=3, scoring='f1_macro')
+    print("3-fold cv F1", scores)
+    print("3-fold cv F1 mean:", np.mean(scores))
+
     print("--------------------------")
     print
 
+def predict_ols(sociolect_metric): 
+    X, y, y_bin, feature_names = get_data(sociolect_metric)
+    model = sm.OLS(y, X)
+    results = model.fit()
+    print(feature_names)
+    print(results.summary())
 
 def main(): 
-    predict_sociolects('pmi')
-    predict_sociolects('tfidf')
+    #predict_sociolects('pmi')
+    #predict_sociolects('tfidf')
+    predict_ols('pmi')
+    predict_ols('tfidf')
 
 if __name__ == "__main__":
     main()
