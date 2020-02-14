4a647d53f72d4cae4e9a4d34fa9bc77cd010c355
Lucy Li <lucy3_li@berkeley.edu>
2020-02-13 17:14:35 -0800

    moving to sequoia because I'm sad

--- code/semeval2010_eval.sh
+++ code/semeval2010_eval.sh
@@ -2,15 +2,16 @@
 
 module load java
 
-dims=(2 10 20 50 100 150)
-lambs=(1000 10000 15000)
+dims=(2 10 20)
+lambs=(1000 5000 10000 15000)
 for j in "${dims[@]}"
 do
     for k in "${lambs[@]}"
     do
         rm fscores.temp
-        rm vmeasures.temp 
-        for i in {0..9}
+        rm vmeasures.temp
+        rm average.temp 
+        for i in {0..4}
         do
             fscore=$(java -jar ../semeval-2010-task-14/evaluation/unsup_eval/fscore.jar ../logs/semeval2010/semeval2010_clusters${j}_${i}_${k} ../semeval-2010-task-14/evaluation/unsup_eval/keys/all.key all | tail -1)
 
@@ -18,8 +19,10 @@ do
 
             IFS=':' read -ra fscore_parts <<< "$fscore"
             IFS=':' read -ra vmeasure_parts <<< "$vmeasure"
+            avg=$(awk "BEGIN {print (${fscore_parts[1]}+${vmeasure_parts[1]})/2; exit}")
             echo ${fscore_parts[1]} >> "fscores.temp"
             echo ${vmeasure_parts[1]} >> "vmeasures.temp"
+            echo $avg >> "average.temp"
         done
         echo "Scores for dimension $j, lambda $k"
         fscore_mean=$(cat fscores.temp | awk '{sum+=$1}END{print sum/NR}')
@@ -28,6 +31,10 @@ do
         vmeasure_mean=$(cat vmeasures.temp | awk '{sum+=$1}END{print sum/NR}')
         vmeasure_std=$(cat vmeasures.temp | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}')
         echo "Vmeasure:" $vmeasure_mean, $vmeasure_std
+        average_mean=$(cat average.temp | awk '{sum+=$1}END{print sum/NR}')
+        average_std=$(cat average.temp | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}')
+        echo "Average:" $average_mean, $average_std
+ 
     done
 done
 
