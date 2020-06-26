34b7ef3a6458861d906431a81fd5af278ec65242
Lucy Li <lucy3_li@berkeley.edu>
2020-06-26 23:19:36 +0000

    tokenizer

--- code/tokenizer_helper.py
+++ code/tokenizer_helper.py
@@ -1,7 +1,6 @@
 '''
 Tokenizes using BERT BasicTokenizer
 and removes duplicates for each user.
-
 Use this instead of stanford_tokenizer.sh and remove_duplicates.sh
 '''
 
@@ -17,8 +16,6 @@ def main():
     outfile = open(outpath, 'w') 
     tokenizer = BasicTokenizer(do_lower_case=True)
     vocab = set()
-    with open(ROOT + 'logs/all_mwes.json', 'r') as infile:
-        mwe_dict = json.load(infile)
     with open(filename, 'r') as infile: 
         for line in infile: 
             if line.startswith('USER1USER0USER'): 
@@ -27,40 +24,7 @@ def main():
                 vocab = set()
             else: 
                 tokens = tokenizer.tokenize(line.strip())
-                new_tokens = []
-                i = 0
-                while i < len(tokens): 
-                    w = tokens[i]
-                    if w in mwe_dict and i + 1 < len(tokens): 
-                       curr_layer = mwe_dict[w]
-                       curr = i + 1
-                       res = w + ' '
-                       poss_complete = None
-                       poss_i = curr
-                       while curr < len(tokens) and tokens[curr] in curr_layer:
-                          if '$END_TOKENS$' in curr_layer: 
-                              poss_complete = res
-                              poss_i = curr
-                          res += tokens[curr] + ' ' 
-                          curr_layer = curr_layer[tokens[curr]] 
-                          curr += 1
-                       if '$END_TOKEN$' in curr_layer: 
-                          poss_complete = res
-                          poss_i = curr
-                       if poss_complete is not None: 
-                          i = poss_i
-                          new_tokens.append(poss_complete.strip())
-                       else: 
-                          i += 1
-                          new_tokens.append(w)
-                    else: 
-                       i += 1
-                       new_tokens.append(w)
-                # for each word, check if it's the beginning of a mwe
-                # the MWE dictionary is of format word1 -> word2 -> $END_TOKEN$
-                # if the next word isn't part of the dictionary, check for end token
-                # the end token might appear in an earlier branch 
-                vocab.update(new_tokens)
+                vocab.update(tokens)
         if len(vocab) > 0: 
             outfile.write('\n'.join(list(vocab)) + '\n')
     outfile.close()    
