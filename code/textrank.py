8ead05cba9ddc45ae0ba41ea7ccd250331adcec3
Lucy Li <lucy3_li@berkeley.edu>
2021-01-05 03:36:27 +0000

    moving to savio for parallel

--- code/textrank.py
+++ code/textrank.py
@@ -32,41 +32,36 @@ def main():
     tokenizer = BasicTokenizer(do_lower_case=True)
     nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
     nlp.tokenizer = CustomTokenizer(nlp.vocab)
-    for article in os.listdir(INFOLDER):
-        start = time.time()
-        print("creating graph for", article) 
-        G = nx.Graph()
+    article = sys.argv[1]
+    
+    print("creating graph for", article) 
+    G = nx.Graph()
 
-        infile = open(INFOLDER + article + '/RC_sample', 'r') 
-        for line in infile: 
-            if line.startswith('USER1USER0USER'): continue
-            doc = nlp(line)
-            comment_toks = []
-            for token in doc: 
-                if token.pos_ in set(['ADJ', 'NOUN', 'PROPN']): 
-                     comment_toks.append(token.text.lower())
-            
-            # add edges to graph using window size of 2
-            for i in range(len(comment_toks) - 1): 
-                w1 = comment_toks[i]
-                w2 = comment_toks[i + 1]
-                if not G.has_edge(w1, w2):
-                    G.add_edge(w1, w2)
-        infile.close()
-        print("TIME:", time.time() - start)
-        print("calculating pagerank...")
-        pagerank_scores = nx.pagerank(G, alpha=0.85, tol=0.0001)
-        outfile = open(OUTFOLDER + article, 'w')
-        writer = csv.writer(outfile)
-        writer.writerow(['word', 'textrank'])
-        vals = sorted(pagerank_scores.items(), key=lambda item: item[1])
-        for tup in vals: 
-            writer.writerow([tup[0], tup[1]])
-        outfile.close()
-        
-        print("TOTAL TIME:", time.time() - start)
-        break
-            
+    infile = open(INFOLDER + article + '/RC_sample', 'r') 
+    for line in infile: 
+	if line.startswith('USER1USER0USER'): continue
+	doc = nlp(line)
+	comment_toks = []
+	for token in doc: 
+	    if token.pos_ in set(['ADJ', 'NOUN', 'PROPN']): 
+		 comment_toks.append(token.text.lower())
+	
+	# add edges to graph using window size of 2
+	for i in range(len(comment_toks) - 1): 
+	    w1 = comment_toks[i]
+	    w2 = comment_toks[i + 1]
+	    if not G.has_edge(w1, w2):
+		G.add_edge(w1, w2)
+    infile.close()
+    pagerank_scores = nx.pagerank(G, alpha=0.85, tol=0.0001)
+    outfile = open(OUTFOLDER + article, 'w')
+    writer = csv.writer(outfile)
+    writer.writerow(['word', 'textrank'])
+    vals = sorted(pagerank_scores.items(), key=lambda item: item[1])
+    for tup in vals: 
+	writer.writerow([tup[0], tup[1]])
+    outfile.close()
+
 
 if __name__ == "__main__":
     main()
